a5b0b259bf46a45a8270ce115101b5230c721c6b
Noah Medoff <noahmedoff@gmail.com>
2024-01-22 06:05:17 -0800

    create RTKQuery api endpoint mutations and user slice state

---
+++ frontend/src/redux/features/auth/userSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = { user: null, token: null }
+
+const userSlice = createSlice({
+    name: auth,
+    initialState,
+    reducers: {
+        setUser(
+            state,
+            { payload: { user, token } }
+        ) {
+            state.user = user
+            state.token = token
+        },
+        restoreUser(
+            state,
+            { payload: { user, token } }
+        ) {
+            state.user = user
+            state.token = token
+        },
+        removeUser(state) {
+            state.user = null
+            state.token = null
+        }
+    }
+})
+
+
+export const { setUser, restoreUser, removeUser } = userSlice.actions
+
+export default userSlice.reducer
