c235014d0be56412542c390838bf369deb492d8a
roysa <roysapeguero@outlook.com>
2024-01-31 13:03:48 -0800

    Test create blog route

--- backend/routes/api/blog.js
+++ backend/routes/api/blog.js
@@ -44,4 +44,41 @@ router.get(
   })
 );
 
+// Get details of a Blog from id
+router.get("/:blogId", async (req, res, next) => {
+  let blogId = req.params.blogId;
+  let blog = await Blog.findByPk(blogId);
+  if (!blog) {
+    const err = new Error("Blog not found");
+    err.status = 404;
+    err.title = "Blog not found";
+    err.errors = ["Blog could not be found"];
+    return next(err);
+  }
+
+  return res.json(blog);
+});
+
+// Create a blog
+router.post(
+  "/",
+  // requireAuth,
+  async (req, res, next) => {
+    const { title, body, thumbnail, tags } = req.body;
+    const userId = req.user.id;
+    console.log(userId);
+
+    const newBlog = await Blog.create({
+      title,
+      body,
+      tags,
+      thumbnail,
+      userId,
+    });
+
+    res.status(201);
+    return res.json(newBlog);
+  }
+);
+
 module.exports = router;
